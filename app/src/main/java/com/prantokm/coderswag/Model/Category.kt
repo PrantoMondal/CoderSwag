d0b45cb6338273db0e57869ae06583bb04d4354e
PrantoMondal <prantomondal517@gmail.com>
2021-11-22 01:18:01 +0600

    List View added

--- app/src/main/java/com/prantokm/coderswag/Model/Category.kt
+++ app/src/main/java/com/prantokm/coderswag/Model/Category.kt
@@ -1,4 +1,9 @@
 package com.prantokm.coderswag.Model
 
-class Category(val title:String,val image: String)
+class Category(val title:String,val image: String){
+    override fun toString(): String {
+        return title
+    }
+
+}
 
